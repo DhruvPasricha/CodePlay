228c58fbf9db852479818af31e2b54703a0a3915
DhruvPasricha <78498002+DhruvPasricha@users.noreply.github.com>
2021-10-20 04:53:56 +0530

    added editor and resize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Editor from './Editor'
+import { useState } from 'react'
+import ColumnResizer from "react-column-resizer";
 
-function App() {
+export default function App() {
+
+
+  const [htmlValue, setHtml] = useState("<h1>Hello World!</h1>");
+  const [cssValue, setCSS] = useState(`body{
+    color : red;
+}`)
+  const [jsValue, setJs] = useState(`console.log("Hello World")`);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <center>
+      <div className="pane top-pane">
+        <table>
+          <tbody>
+            <tr>
+              <Editor
+                displayName="HTML"
+                lang="xml"
+                value={htmlValue}
+                setValue={setHtml}
 
-export default App;
+              />
+
+              <ColumnResizer
+                className="columnResizer resizer"
+                minWidth={0}
+
+              />
+
+              <Editor
+                displayName="CSS"
+                lang="css"
+                value={cssValue}
+                setValue={setCSS}
+
+              />
+
+
+              <ColumnResizer
+                className="columnResizer resizer"
+                minWidth={0}
+
+              />
+
+              <Editor
+                displayName="JS"
+                lang="javascript"
+                value={jsValue}
+                setValue={setJs}
+
+              />
+
+            </tr>
+
+          </tbody>
+        </table>
+      </div>
+      <div className="pane">
+        <iframe
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%">
+        </iframe>
+      </div>
+    </center>
+  )
+}
